38ef3bb72da42c8121eeb95760a225d983262833
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-14 21:25:48 -0500

    bunch o' comments

--- examples/stringtable.s
+++ examples/stringtable.s
@@ -7,39 +7,64 @@ ARGC  = $f002
 
 .ZEROPAGE
 
+; Define some zeropage address locations. These aren't included in the
+; assembled binary, but it allows us to refer to the locations by name
+; in the CODE segment.
+
 addrlo:         .byte 0
 addrhi:         .byte 0
 
 .CODE
-
-                lda ARGC
+                lda ARGC                ; use ARGC (number of command line
+                                        ; arguments) as the error code.
                 jsr strerror
-                stp
+                stp                     ; exit the program
 
-strerror:       asl a
+; @name strerror
+;
+; Returns the address of a string describing a numeric error code.
+;
+; @param a -- error code
+; @return none
+strerror:       asl a                   ; multiply error code by two because
+                                        ; error_table consists of two-byte
+                                        ; entries
+
                 cmp #(error_max-error_table)
+                                        ; make sure error code is one
+                                        ; we know about
                 bcc continue
                 lda #(error_max-error_table)
+                                        ; load last error message, which
+                                        ; should be the "invalid error
+                                        ; code" message
 
-continue:       tax
-                lda error_table,x
+continue:       tax                     ; move value to x register for indexing
+                lda error_table,x       ; load low byte of string address
                 sta addrlo
                 inx
-                lda error_table, x
+                lda error_table, x      ; load high byte of string address
                 sta addrhi
-                jsr println
+                jsr println             ; print the error message
                 rts
 
+; @name println
+;
+; Print a string, terminiating it with <cr><lf>.
+;
+; @param addrlo zero page location of low byte of string address
+; @param addrhi zero page location of high byte of string address
+; @return none
 println:        ldy #0
-@loop:          lda ($0),y
+@loop:          lda (addrlo),y
                 sta STDIO
-                beq @end
+                beq @end                ; exit on termintal 0 
                 iny
                 jmp @loop
                
-@end:           lda #$0d
+@end:           lda #$0d                ; send <cr>
                 sta STDIO
-                lda #$0a
+                lda #$0a                ; send <lf>
                 sta STDIO
                 lda #0
                 rts
