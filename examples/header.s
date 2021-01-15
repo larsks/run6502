937677b6a3066aab9c49906a50b589cd735848ff
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-14 21:34:46 -0500

    add comments on header.s

--- examples/header.s
+++ examples/header.s
@@ -1,11 +1,14 @@
+; This file describes the header used by run6502 to correctly
+; load and execute compiled 6502 binaries.
+
 .segment "SIGNATURE"
 
-        .byte "RN65"
+        .byte "RN65"            ; embed signature into generated output
 
-.segment        "LOADADDR"
+.segment "LOADADDR"
 
-        .word *+4
+        .word *+4               ; embed load address
 
 .segment "STARTADDR"
 
-        .word *+2
+        .word *+2               ; embed start address
