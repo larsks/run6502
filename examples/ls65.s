d056df161afb3ede7ca038bdeef4e79957ce6d31
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-15 19:10:19 -0500

    refactor code to have a listdir subroutine

--- examples/ls65.s
+++ examples/ls65.s
@@ -18,30 +18,42 @@ addrhi: .byte 0
 
 .CODE
 
-        ldx #0
+main:   ldx #0
         cpx ARGC                        ; check if there are any arguments
-        bne args                        ; loop over arguments if available
+        bne loop                        ; loop over arguments if available
 
         lda #'.'                        ; default to listing '.' if no args
         sta STRING_BASE
         dex                             ; inx at bottom of loop will set x=0
-        jmp printd
+        jmp lbody
 
-args:   cpx ARGC                        ; check if there are any more arguments
+loop:   cpx ARGC                        ; check if there are any more arguments
         beq end
         stx ARGV                        ; request the next argument
 
-printd: lda #<STRING_BASE               ; load name into addrlo + addrhi
+lbody:  lda #<STRING_BASE               ; load name into addrlo + addrhi
         sta addrlo
         lda #>STRING_BASE
         sta addrhi
         jsr println
+        jsr listdir
+        inx
+        jmp loop                        ; process next argument
 
-dirent: lda #'L'
+end:
+        stp                             ; exit program
+
+; @name listdir
+;
+; Print out the type and name of items in a directory.
+;
+; @param STRING_BASE contains name of directory
+listdir:
+        lda #'L'
         sta DIROPT                      ; request directory listing
         lda DTYPE                       ; read d_type of next entry
                                         ; (places d_name into STRING_BASE)
-        beq next                        ; exit if we have read last dir entry
+        beq @end                        ; exit if we have read last dir entry
         sta STDIO                       ; write dtype to stdout
         lda #' '
         sta STDIO
@@ -51,16 +63,17 @@ dirent: lda #'L'
         lda #>STRING_BASE
         sta addrhi
         jsr println                     ; print name + eol
+        jmp listdir
 
-        jmp dirent
+@end:   rts
 
-next:
-        inx
-        jmp args                        ; process next argument
-
-end:
-        stp                             ; exit program
-
+; @name println
+;
+; Print a string, terminiating it with <cr><lf>.
+;
+; @param addrlo zero page location of low byte of string address
+; @param addrhi zero page location of high byte of string address
+; @return none
 println:        ldy #0                  ; loop over string pointed to by
 @loop:          lda (addrlo),y          ; addrlo + addrhi
                 beq @end                ; exit on termintal 0 
