7325a47db26f64cfd8c9e4ca072b4d214b08428a
Lars Kellogg-Stedman <lars@oddbit.com>
2021-01-19 19:30:18 -0500

    .segment is a pseudo-op, not label

--- examples/header.s
+++ examples/header.s
@@ -1,14 +1,11 @@
 ; This file describes the header used by run6502 to correctly
 ; load and execute compiled 6502 binaries.
 
-.segment "SIGNATURE"
+                .segment        "SIGNATURE"
+                .byte           "RN65"          ; embed signature into generated output
 
-        .byte "RN65"            ; embed signature into generated output
+                .segment        "LOADADDR"
+                .word           *+4             ; embed load address
 
-.segment "LOADADDR"
-
-        .word *+4               ; embed load address
-
-.segment "STARTADDR"
-
-        .word *+2               ; embed start address
+                .segment        "STARTADDR"
+                .word           *+2             ; embed start address
